012d7bb03f5aaec8485a7469ed1dfb40c95db991
TomasJLuis <tomasjluis@gmail.com>
2015-04-07 20:16:37 +0200

    Update init.lua
    I've added these functions to detect if player is placing torches in water.
    Then you call iswet(pos) from 'on_place' and if it's true then avoid placing torch.

--- init.lua
+++ init.lua
@@ -10,3 +10,25 @@ if style == "minecraft" then
 else
 	dofile(modpath.."/mt_style.lua")
 end
+
+-- functions to detect placing in water
+function iswater (pos)
+	local n = minetest.get_node(pos)
+	if minetest.get_item_group(n.name, "water") ~= 0 then return true end --is water
+	return false
+end
+function iswet (pos)
+	local p0 = pos
+	local p1 = {x = p0.x - 1, y = p0.y - 0.5, z = p0.z}
+	local p2 = {x = p0.x + 1, y = p0.y - 0.5, z = p0.z}
+	local p3 = {x = p0.x, y = p0.y - 0.5, z = p0.z + 1}
+	local p4 = {x = p0.x, y = p0.y - 0.5, z = p0.z - 1}
+	if iswater(p0) == true
+	or iswater(p1) == true
+	or iswater(p2) == true
+	or iswater(p3) == true
+	or iswater(p4) == true then	
+		return true -- is wet
+	end
+	return false 	-- all dry
+end
