3c10c1066d33be434d383af37789d0c03eeaae13
TomasJLuis <tomasjluis@gmail.com>
2015-04-09 18:08:05 +0200

    Update init.lua
    Modification to fix flowing water detection and also to extend the function to detect any liquid block, not only water.

--- init.lua
+++ init.lua
@@ -11,10 +11,12 @@ else
 	dofile(modpath.."/mt_style.lua")
 end
 
--- functions to detect placing in water
-function iswater (pos)
+-- functions to detect if there is liquid around pos
+-- used to avoid placing torches in water or lava
+function isliquid (pos)
 	local n = minetest.get_node(pos)
-	if (minetest.get_item_group(n.name, "water") ~= 0 and n.param2 > 0) then return true end
+	local liquidtype = minetest.registered_nodes[n.name].liquidtype or " "
+	if ((liquidtype == "flowing" and n.param2 > 0) or liquidtype == "source") then return true end
 	return false
 end
 function iswet (pos)
@@ -24,6 +26,10 @@ function iswet (pos)
 	local p2 = {x = pos.x + 1, y = pos.y, z = pos.z}
 	local p3 = {x = pos.x, y = pos.y, z = pos.z + 1}
 	local p4 = {x = pos.x, y = pos.y, z = pos.z - 1}
-	-- returns true if it's wet around pos
-	return iswater(pi) or iswater(p0) or iswater(p1) or iswater(p2) or iswater(p3) or iswater(p4)
+	return isliquid(pi)
+		or isliquid(p0)
+		or isliquid(p1)
+		or isliquid(p2)
+		or isliquid(p3)
+		or isliquid(p4)
 end
